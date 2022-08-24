53b78595dfb15489eccae837b0b56c30a5b48117
arual_zeravla <laura.q.alvarez@gmail.com>
2022-08-24 20:47:58 -0300

    primer commit react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <header>
+        <nav class="navbar navbar-expand-md nav_headerContainer">
+            <div class="container-fluid">
+                {/* <a class="navbar-brand" href="index.html"><img class="img_logo" src="img/Logo.jpg" alt="LA BIBLIOTECA"></img></a> */}
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarsExample04">
+                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.html">INICIO</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="promociones.html">MENÚ</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="promociones.html">PARA REGALAR</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="promociones.html">COMBO SEMANA</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">PEDIDOS</a>
+                        </li>
+
+
+                    </ul>
+                    <form role="search">
+                        <input class="form-control" type="search" placeholder="Buscar" aria-label="Search"></input>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <div id="imgDiv">
+    <img src="./images/ad1-bigpic.jpg" alt="image" className="ad-img" /> 
     </div>
+    </>
   );
 }
 
